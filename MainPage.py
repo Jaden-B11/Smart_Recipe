108941584d4dbd88e094d71875e63a15285054c3
Z-ABBASI <101zabbasi@gmail.com>
2024-12-02 17:06:59 -0800

    The skeleton of the routes page, including / and /recipe

--- MainPage.py
+++ MainPage.py
@@ -1,1 +1,14 @@
-print('hello world')
+# hello_flask.py
+from flask import Flask, render_template
+
+# create an instance of Flask
+app = Flask(__name__)
+
+# route decorator binds a function to a URL
+@app.route('/')
+def search():
+  return render_template('search.html')
+
+@app.route('/recipe')
+def recipe():
+  return render_template('recipe.html')
